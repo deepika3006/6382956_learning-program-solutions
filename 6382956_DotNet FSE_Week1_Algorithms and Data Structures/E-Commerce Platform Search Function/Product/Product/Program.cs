7ef20f139921b5530137863200c3eae9153baf4c
deepika3006 <163133783+deepika3006@users.noreply.github.com>
2025-06-22 19:36:01 +0530

    Add files via upload

---
+++ 6382956_DotNet FSE_Week1_Algorithms and Data Structures/E-Commerce Platform Search Function/Product/Product/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+class Product
+{
+    public string name;
+    public string category;
+    public double price;
+
+    public Product(string n, string c, double p)
+    {
+        name = n;
+        category = c;
+        price = p;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+    
+        Product[] products = new Product[5];
+        products[0] = new Product("Laptop", "Electronics", 50000);
+        products[1] = new Product("Mobile", "Electronics", 25000);
+        products[2] = new Product("Shirt", "Clothing", 1500);
+        products[3] = new Product("Shoes", "Footwear", 2000);
+        products[4] = new Product("Jeans", "Clothing", 1800);
+        
+        Console.WriteLine("Welcome to our mini E-Commerce Search!");
+        Console.WriteLine("Available categories: Electronics, Clothing, Footwear");
+        Console.Write("Enter a category to search: ");
+        string? searchCategory = Console.ReadLine();
+
+        if (string.IsNullOrWhiteSpace(searchCategory))
+        {
+            Console.WriteLine("You didn't enter a valid category.");
+            return;
+        }
+
+        Console.WriteLine("\nProducts in category: " + searchCategory);
+        int count = 0;
+
+        for (int i = 0; i < products.Length; i++)
+        {
+            if (products[i].category.ToLower() == searchCategory.ToLower())
+            {
+                Console.WriteLine("- " + products[i].name + " : ₹" + products[i].price);
+                count++;
+            }
+        }
+
+        if (count == 0)
+        {
+            Console.WriteLine("No products found in this category.");
+        }
+        else
+        {
+            Console.WriteLine("\nTotal products found: " + count);
+        }
+    }
+}
---
+++ 6382956_DotNet FSE_Week1_Algorithms and Data Structures/Financial Forecasting/FinancialForecasting/FinancialForecasting/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+class FinancialForecasting
+{
+    static void Main()
+    {
+        List<double> expenses = new List<double>();
+        Console.WriteLine("Enter your monthly expenses for the last 6 months:");
+
+        for (int i = 1; i <= 6; i++)
+        {
+            Console.Write("Month " + i + " expense (₹): ");
+            string? input = Console.ReadLine();
+            double value;
+
+            if (double.TryParse(input, out value))
+            {
+                expenses.Add(value);
+            }
+            else
+            {
+                Console.WriteLine("Invalid input, try again.");
+                i--; 
+            }
+        }
+
+        Console.WriteLine("\nYour Entered Expenses:");
+        for (int i = 0; i < expenses.Count; i++)
+        {
+            Console.WriteLine("Month " + (i + 1) + ": ₹" + expenses[i]);
+        }
+
+        if (expenses.Count >= 2)
+        {
+            double last = expenses[expenses.Count - 1];
+            double secondLast = expenses[expenses.Count - 2];
+            double change = last - secondLast;
+            double trendForecast = last + change;
+            Console.WriteLine("\n Trend-based Forecast for next month: ₹" + Math.Round(trendForecast, 2));
+        }
+
+        if (expenses.Count >= 3)
+        {
+            double sum = 0;
+            for (int i = expenses.Count - 3; i < expenses.Count; i++)
+            {
+                sum += expenses[i];
+            }
+            double movingAvg = sum / 3;
+            Console.WriteLine("3-Month Moving Average Forecast: ₹" + Math.Round(movingAvg, 2));
+        }
+
+        Console.WriteLine("\nThank you for using the forecast tool!");
+    }
+}
